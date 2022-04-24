fc7ec8d1d64186435ef604d50b15ff5748443588
JarrodHelmers <jarrodhelmers@gmail.com>
2022-04-24 09:22:24 -0500

    Tasks completed

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,55 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+const VehicleModule = require("./vehicle").Vehicle
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+class Car extends VehicleModule{
+  constructor(make, model, year, color, mileage){
+    super(make, model, year, color, mileage);
+    this.maxPassengers = 5;
+    this.passenger = 0;
+    this.numberOfWheels = 4;
+    this.maximumSpeed = 160;
+    this.fuel = 10;
+    this.scheduleService = false;
+  }
+
+  checkService(){
+    if (this.mileage > 30000) {
+      this.scheduleService = true
+      return this.scheduleService;
+    }
+  }
+
+  start() {
+    if (this.fuel > 0) {
+      console.log("engine has started.");
+      return this.started = true
+    }
+    else {
+      console.log("no fuel")
+      return this.started = false
+    }
+  }
+
+  loadPassenger(num) {
+    if (this.passenger < this.maxPassengers) {
+      if ((num + this.passenger) <= this.maxPassengers) {
+        this.passenger = num;
+        return this.passenger
+      }
+      else {
+        console.log (this.model + " " + this.make + " does not have enough space for all passengers.")
+        }   
+      }
+    else {
+      console.log(this.model + " " + this.make + " is full");
+    }
+  }
+}
+
+let myCar = new Car('mercury', 'sedan', '1965', 'red', 40000)
+
+myCar.start()
+myCar.loadPassenger(4)
+myCar.stop()
+myCar.checkService()
+
+console.log(myCar)
